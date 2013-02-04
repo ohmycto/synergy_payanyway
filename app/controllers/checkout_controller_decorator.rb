93f06755acbe96caa56cd5deae259f2486594910
Slava Epifanov <slava.epifanov@gmail.com>
2013-02-04 17:17:14 +0400

    it should save choosen payment method

--- app/controllers/checkout_controller_decorator.rb
+++ app/controllers/checkout_controller_decorator.rb
@@ -7,6 +7,7 @@ CheckoutController.class_eval do
     return unless params[:state] == 'payment'
     payment_method = PaymentMethod.find(params[:order][:payments_attributes].first[:payment_method_id])
     if payment_method.kind_of? Gateway::Payanyway
+      @order.update_attributes(object_params)
       redirect_to(gateway_payanyway_path(:gateway_id => payment_method.id, :order_id => @order.id))
     end
   end
